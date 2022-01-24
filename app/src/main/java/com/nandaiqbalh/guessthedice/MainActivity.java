75634898d2ddab8cf04ab8d35aac577e30e388da
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-01-24 12:26:40 +0700

    Rolling the Dice

--- app/src/main/java/com/nandaiqbalh/guessthedice/MainActivity.java
+++ app/src/main/java/com/nandaiqbalh/guessthedice/MainActivity.java
@@ -3,7 +3,13 @@ package com.nandaiqbalh.guessthedice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,37 @@ public class MainActivity extends AppCompatActivity {
 
         // full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        Button myButton = findViewById(R.id.rollButton);
+
+        final int[] diceArray = new int[] {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                Log.d("Dicee", "The number for the left dice is " + number);
+
+                int imageResourceId = diceArray[number];
+                leftDice.setImageResource(imageResourceId);
+
+                number = randomNumberGenerator.nextInt(6);
+                Log.d("Dicee", "The number for the right dice is " + number);
+                imageResourceId = diceArray[number];
+                rightDice.setImageResource(imageResourceId);
+            }
+        });
+
     }
 }
